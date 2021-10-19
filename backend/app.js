709e3cf6e7be33b1a590a0a3580a6f68346f3d26
Camille Bianco <bianco.camille@live.fr>
2021-10-19 10:06:19 +0200

    Connexion à la BDD de mongoDB

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,17 @@ const bodyParser = require('body-parser');
 //Importer mongoose
 const mongoose = require('mongoose');
 
+//Connecter application à BDD Mongoose
+mongoose.connect('mongodb+srv://Camille:yhLhpUwAl33Wp6nM@cluster0.18oz4.mongodb.net/Cluster0?retryWrites=true&w=majority', 
+{useNewUrlParser : true, 
+useUnifiedTopology : true})
+    .then(function(){
+        console.log('Connexion à MongoDB réussi.')
+    })
+    .catch(function(error){
+        console.log('Connexion à MongoDB échoué.')
+    })
+
 //Créer l'application express
 const app = express(); 
 
